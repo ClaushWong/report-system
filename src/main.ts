118f9f4a6370d30db0b9c8d2b7651f23056a1804
Claush Wong <claushwong123@gmail.com>
2023-04-16 20:37:28 +0800

    modify main

--- src/main.ts
+++ src/main.ts
@@ -8,26 +8,27 @@ import { APP_PORT, CORS_ORIGINS, APP_ENVIRONMENT } from "./config";
 const logger = new Logger("bootstrap");
 
 async function bootstrap() {
-  const corsOrigin =
-    APP_ENVIRONMENT !== "development"
-      ? CORS_ORIGINS
-        ? CORS_ORIGINS.split(",").map((c) => c.trim())
-        : []
-      : "*";
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    const corsOrigin =
+        APP_ENVIRONMENT !== "development"
+            ? CORS_ORIGINS
+                ? CORS_ORIGINS.split(",").map((c) => c.trim())
+                : []
+            : "*";
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.use(bodyParser.json({ limit: "25mb" }));
-  app.use(bodyParser.urlencoded({ limit: "25mb", extended: true }));
-  app.useGlobalPipes(new ValidationPipe());
+    app.use(bodyParser.json({ limit: "25mb" }));
+    app.use(bodyParser.urlencoded({ limit: "25mb", extended: true }));
+    app.useGlobalPipes(new ValidationPipe());
 
-  app.enableCors({
-    origin: corsOrigin,
-  });
+    app.enableCors({
+        origin: corsOrigin,
+    });
 
-  await app.listen(APP_PORT, "0.0.0.0");
+    await app.listen(APP_PORT, "0.0.0.0");
 
-  logger.log(`cors origin: ${CORS_ORIGINS}`);
-  logger.log(`application is running on: ${await app.getUrl()}`);
+    logger.log(`current cors:`, corsOrigin);
+    logger.log(`cors origin: ${CORS_ORIGINS}`);
+    logger.log(`application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
